af6eaaa6f51037439d51d464b3e11ccc4d9eea44
Si1mT <siim.tanavots@tptlive.ee>
2019-06-05 14:02:49 +0300

    idk just for safety

--- Snake_game_t118e/MainWindow.xaml.cs
+++ Snake_game_t118e/MainWindow.xaml.cs
@@ -32,6 +32,7 @@ namespace Snake_game_t118e
         public void Timer_Tick(object sender, EventArgs e)
         {
             tick(dir);
+            
         }
 
         private DispatcherTimer timer;
@@ -93,6 +94,7 @@ namespace Snake_game_t118e
             else if (dire % 4 == 1) y += 25;
             else if (dire % 4 == 2) x -= 25;
             else if (dire % 4 == 3) y -= 25;
+            if (y > 450 || y < 0 || x > 450 || x < 0) end();
                 Canvas.SetLeft(Snek, x);
                 Canvas.SetBottom(Snek, y);
         }
